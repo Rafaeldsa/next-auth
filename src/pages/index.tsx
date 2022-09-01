06bc9a048be5ab477ab2763bdebeb59e0b88ed43
rafael.azevedo <rafael.azevedo@virtus.ufcg.edu.br>
2022-09-01 08:00:01 -0300

    update

---
+++ src/pages/index.tsx
@@ -0,0 +1,35 @@
+import { FormEvent, useContext, useState } from 'react';
+import styles from '../../styles/Home.module.css';
+import { AuthContext } from '../contexts/AuthContext';
+
+export default function Home() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const { signIn } = useContext(AuthContext);
+
+  async function handleSubmit(e: FormEvent) {
+    e.preventDefault();
+
+    const data = {
+      email,
+      password
+    }
+
+    await signIn(data);
+  }
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.main}>
+      <form onSubmit={handleSubmit} className={styles.container}>
+      <input type="email" value={email} onChange={e => setEmail(e.target.value)}/>
+      <input type="password" value={password} onChange={e => setPassword(e.target.value)}/>
+      <button type="submit">Entrar</button>
+    </form>
+    </div>
+    </div>
+    
+    
+  )
+}
